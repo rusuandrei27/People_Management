dfdafaf9997df83506db2581891d0eda1357bd93
rusuandrei27 <andreirusuraiciu@gmail.com>
2025-07-18 15:27:47 +0300

    Finished register logic

--- FrontEnd/auth/otp/otp.js
+++ FrontEnd/auth/otp/otp.js
@@ -8,38 +8,66 @@ $('.otp-box').on('input', function () {
 $("#waitingPanel").dxLoadPanel({
     shadingColor: "rgba(0,0,0,0.4)",
     message: "Please wait...",
-    visible: false, // start hidden
+    visible: false,
     showIndicator: true,
     showPane: true,
     shading: true,
     hideOnOutsideClick: false,
 });
 
-$('#confirmOtp').on('click', () => {
-    let otp = '';
-    $('.otp-box').each((index, item) => {
-        otp += $(item).val();
-    });
+$("#register-btn").dxButton({
+    text: "Confirm",
+    type: "default",
+    width: "90%",
+    onClick: async function () {
+        $("#register-btn").dxButton("instance").option("disabled", true);
+        let otp = '';
+        $('.otp-box').each((index, item) => {
+            otp += $(item).val();
+        });
 
-    if (otp.length === 6) {
+        if (otp.length != 6) {
+            $("#register-btn").dxButton("instance").option("disabled", false);
+            DevExpress.ui.notify("Please complete the otp code!", "warning", 2000);
+            return;
+        }
+
         $("#waitingPanel").dxLoadPanel("instance").option("visible", true);
 
-        // const emailResponse = await fetch("http://localhost:3000/api/auth/sendRegisterEmail", {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify(userData)
-        // });
+        let userData = sessionStorage.getItem("userData");
 
-        // const emailJSON = await emailResponse.json();
+        if (!userData) {
+            $("#waitingPanel").dxLoadPanel("instance").option("visible", false);
+            DevExpress.ui.notify("The register session expired. Please try again!", "warning", 2000);
+            return;
+        }
 
+        userData = JSON.parse(userData);
+        userData["otp"] = otp;
 
-        return;
-        // console.log("OTP:", otp);
-        // You can send it to backend here
-        // e.g., fetch('/api/auth/verifyOtp', ...)
+        const otpResponse = await fetch("http://localhost:3000/api/auth/insertUser", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userData)
+        });
+
+        const otpJSON = await otpResponse.json();
+
+        if (!otpResponse || !otpResponse.ok || otpResponse.error) {
+            $("#waitingPanel").dxLoadPanel("instance").option("visible", false);
+            DevExpress.ui.notify(otpJSON.error, "error", 2000);
+            $("#register-btn").dxButton("instance").option("disabled", false);
+            return;
+        }
+
+        sessionStorage.removeItem("userData");
+
+        DevExpress.ui.notify("Your account has been successfully created. You are now redirected to login page!", "warning", 3000);
+
+        setTimeout(function () {
+            window.location.href = '../login/login.html';
+        }, 2000);
     }
-
-    DevExpress.ui.notify("Please complete OTP code!", "warning", 2000);
 });
